ac9afe9de8d631d97a574d0717bce1a562343ffe
Mellllow <131720670+Mellllow@users.noreply.github.com>
2023-06-13 15:00:05 -0700

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -1,23 +1,23 @@
-var button = document.querySelector(".button");
-var imputValue = document.querySelector(".imputValue");
+var button = document.getElementById("test");
+var inputValue = document.querySelector(".inputValue");
 var main = document.querySelector('.name');
 var description = document.querySelector('.description');
-var temprature = document.querySelector('.temprature');
+var temperature = document.querySelector('.temperature');
 
 
 
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+imputValue.value+'&appid=4ad42e2953e7e34fc026293c0648c39d')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=e4b245f8023f51c13bc5bf3f9ee3c7c1')
 
 .then(response => response.json())
 .then ( data=> {
-    var nameValue =data["main"];
-    var tempratureValue = data["main"]["temprature"];
+    var nameValue =data["name"];
+    var temperatureValue = data["main"]["temprature"];
     var descriptionValue = data["weather"][0]["description"];
 
         main.innerHTML = nameValue;
-        tempreture.innerHTML = tempratureValue;
+        temperature.innerHTML = temperatureValue;
         description.innerHTML = descriptionValue;
         input.value ="";
 
