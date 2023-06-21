f0f79f072b42e5f8ac9b2f1eba27df0f29a13c69
Mellllow <131720670+Mellllow@users.noreply.github.com>
2023-06-20 21:02:12 -0700

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -8,20 +8,25 @@ var temperature = document.querySelector('.temperature');
 
 
 button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=e4b245f8023f51c13bc5bf3f9ee3c7c1')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=e4b245f8023f51c13bc5bf3f9ee3c7c1')
 
 .then(response => response.json())
 .then ( data=> {
+    console.log(data)
     var nameValue =data["name"];
-    var temperatureValue = data["main"]["temprature"];
+    var temperatureValue = data["main"]["temp"];
     var descriptionValue = data["weather"][0]["description"];
 
         main.innerHTML = nameValue;
         temperature.innerHTML = temperatureValue;
         description.innerHTML = descriptionValue;
-        input.value ="";
+        
 
     })
-.catch (err => alert("Not a locaiton"));
+.catch (err =>{
+    if (err){
+        console.log(err)
+    alert("Not a locaiton")} 
+    });
 })
 
